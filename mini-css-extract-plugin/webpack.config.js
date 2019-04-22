8ddbb522174bfcafd1a80105bae685ffe5ff24a7
George Taveras <gtaveras@xogrp.com>
2019-04-22 17:16:50 -0400

    Remove unnecessary comments

--- mini-css-extract-plugin/webpack.config.js
+++ mini-css-extract-plugin/webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  // entry: './src/index.js',
-  // output: {
-  //   filename: '[name].js',
-  //   path: path.resolve(__dirname, 'dist')
-  // },
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].css'
